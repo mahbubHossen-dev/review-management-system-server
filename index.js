915bd0a0a1971e60c0a01cc54a614dc387d4d999
MahbubHosssen <mhbabu2002@gmail.com>
2024-12-22 13:39:43 +0600

    install express mongodb cors and mongodb setup successfully

---
+++ index.js
@@ -0,0 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+var cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+// zG2zYX8WWfnzjvcC
+// reviewSystem
+
+
+const uri = `mongodb+srv://${process.env.REVIEW_USER}:${process.env.REVIEW_PASS}@cluster0.xdjfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+
+
+        const db = client.db('Review-System')
+        const serviceCollections = db.collection('services')
+
+        
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
