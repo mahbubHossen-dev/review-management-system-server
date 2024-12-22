69c671adeda42bcd14a487f8eb385c0272e2f599
MahbubHosssen <mhbabu2002@gmail.com>
2024-12-22 21:03:33 +0600

    create post api, review post api, and get services api, review api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 var cors = require('cors')
 require('dotenv').config()
@@ -32,8 +32,61 @@ async function run() {
 
         const db = client.db('Review-System')
         const serviceCollections = db.collection('services')
+        const reviewCollections = db.collection('all-reviews')
+        // Post services
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollections.insertOne(service)
+            res.send(result)
+            // console.log(service)
+        })
 
-        
+        // get All Services
+        app.get('/services', async (req, res) => {
+
+            const email = req.query.email
+            let query;
+            if(email){
+                query = {email}
+            }
+
+            const result = await serviceCollections.find(query).toArray()
+            res.send(result)
+        })
+
+
+        // get specific service
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)}
+            const result = await serviceCollections.findOne(filter)
+            res.send(result)
+            console.log(id)
+        })
+
+
+        // post review
+        app.post('/addReview', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollections.insertOne(review)
+            res.send(result)
+            console.log(review)
+        })
+
+        // get All Review
+        app.get('/all-review', async (req, res) => {
+            const result = await reviewCollections.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = {service_id: id}
+            const result = await reviewCollections.find(filter).toArray()
+            res.send(result)
+        })
+
+
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
